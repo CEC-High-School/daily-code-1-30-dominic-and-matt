71b02e476ddc7b81763a6f5fe39bf29752e0b200
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-01-30 13:54:02 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int num = 20;
+int num2 = 11;
+int main()
+{
+	
+
+	for (int i = 20; i <= 80; i++)
+		cout << i << endl;
+
+	system("pause");
+
+	for (int j = 70; j >= 10; j--)
+		cout << j << endl;
+	system("pause");
+
+	while (num < 81) {
+		cout << num << endl;
+		num++;
+	}
+	system("pause");
+		while (num2 < 11) {
+			cout << num2 << endl;
+			num2++;
+}
+		system("pause");
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+
+
+
+
+
+
+
+
+
+
+}
